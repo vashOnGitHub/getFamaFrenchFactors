a7d00cb3ec3c6bb9dc3091e9edcfdc8b9a3728f1
vashOnGitHub <50780753+vashOnGitHub@users.noreply.github.com>
2024-09-25 09:19:38 +0100

    Update getFamaFrenchFactors.py
    This update (courtesy of Valentine Panchenko) adds capability for daily data across factor models.

--- src/getFamaFrenchFactors.py
+++ src/getFamaFrenchFactors.py
@@ -1,14 +1,27 @@
 # getFamaFrenchFactors.py
 # Author: Vash
-# Version 0.0.4
-# Last updated: 18 May 2019
+# Modified by Valentyn Panchenko
+# Version 0.0.6
+# Last updated: 27 October 2023
 
 """
 This programme gets cleaned versions of factors including:
-    * Fama French 3 factor (MRP, SMB, HML)
-    * Momentum (MOM)
-    * Carhart 4 factors (MRP, SMB, HML, MOM)
-    * Fama French 5 factors (MRP, SMB, HML, RMW, CMA)
+    * famaFrench3Factor() -- Fama French 3 factor (MRP, SMB, HML)
+    * momentumFactor() -- Momentum (MOM)
+    * carhart4Factor() -- Carhart 4 factors (MRP, SMB, HML, MOM)
+    * famaFrench5Factor() -- Fama French 5 factors (MRP, SMB, HML, RMW, CMA)
+
+The frequency of the data is controlled by a single parameter
+frequency take values:
+'d' daily (default)
+'m' monthly
+'a' annual
+
+
+Updates in Version 0.0.6:
+Adds support for daily data in addition to annual and monthly data
+DataFrame "date_ff_factors" field changed to more conventional "Date"
+"Date" is harmonised to Pandas date format set to the last day the period (for monthly and annual series)
 
 Updates in Version 0.0.4:
 Replaces manual URL with scraped URL for initial futureproofing.
@@ -28,7 +41,7 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
 
 text_to_search = ['Fama/French 3 Factors', 'Momentum Factor (Mom)']
-all_factors_text = soup.findAll('b', text=text_to_search)
+all_factors_text = soup.findAll('b', string=text_to_search)
 
 home_url = "http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/"
 all_factor_links = []
@@ -48,58 +61,70 @@ for text in all_factors_text:
     factor_dict = {'factor' : text, 'csv_links' : csv_links, 'txt_links' : txt_links}
     all_factor_links.append(factor_dict)
 
-ff3factor_dict = dict(all_factor_links[0])
+ff_factor_dict = dict(all_factor_links[0])
 momAndOthers_dict = dict(all_factor_links[1])
 
 
-def famaFrench3Factor(frequency='m'):
+def famaFrench3Factor(frequency='d'):
     '''
     Returns Fama French 3 factors (Market Risk Premium, SMB, HML)
 
     Set frequency as:
+        'd' for daily factors
         'm' for monthly factors
         'a' for annual factors
 
     '''
     rows_to_skip = 3
-    ff3_raw_data = ff3factor_dict['csv_links'][0]
 
-    ff3_factors = pd.read_csv(ff3_raw_data, skiprows=rows_to_skip)
-    ff3_factors.rename(columns = {ff3_factors.columns[0] : 'date_ff_factors'},
-                       inplace=True)
+    if frequency == 'd':
+        ff3_raw_data = ff_factor_dict['csv_links'][2]
+        ff3_factors = pd.read_csv(ff3_raw_data, skiprows=rows_to_skip)
+        ff3_factors.rename(columns={ff3_factors.columns[0]: 'Date'},
+                           inplace=True)
+        # Ignore copyright footer     
+        ff3_factors = ff3_factors.iloc[:-2]
+        ff3_factors['Date'] = pd.to_datetime(ff3_factors['Date'],
+                                                    format='%Y%m%d')
+    else:
+        ff3_raw_data = ff_factor_dict['csv_links'][0]
+        ff3_factors = pd.read_csv(ff3_raw_data, skiprows=rows_to_skip)
+        ff3_factors.rename(columns={ff3_factors.columns[0]: 'Date'},
+                           inplace=True)
+        
+        # Clean annual and monthly versions
 
-    # Get index of annual factor returns
-    annual_factor_index_loc = ff3_factors[
+        # Get index of annual factor returns
+        annual_factor_index_loc = ff3_factors[
             ff3_factors.values == ' Annual Factors: January-December '].index
 
-    # Clean annual and monthly versions
-    if frequency == 'm':
-        ff3_factors.drop(ff3_factors.index[annual_factor_index_loc[0]:], inplace=True)
+        if frequency == 'm':
+            ff3_factors.drop(ff3_factors.index[annual_factor_index_loc[0]:], inplace=True)
 
-        # Convert dates to pd datetime objects
-        ff3_factors['date_ff_factors'] = pd.to_datetime(ff3_factors['date_ff_factors'],
-                                                        format='%Y%m')
-        # Shift dates to end of month
-        ff3_factors['date_ff_factors'] = ff3_factors['date_ff_factors'].apply(
-            lambda date : date + relativedelta(day = 1, months = +1, days = -1))
+            # Convert dates to pd datetime objects
+            ff3_factors['Date'] = pd.to_datetime(ff3_factors['Date'],
+                                                            format='%Y%m')
+            # Shift dates to end of month
+            ff3_factors['Date'] = ff3_factors['Date'].apply(
+                lambda date : date + relativedelta(day = 1, months = +1, days = -1))
 
-    elif frequency == 'a':
-        # Extract annual data only
-        ff3_factors.drop(ff3_factors.index[:annual_factor_index_loc[0]],
-                         inplace=True)
+        elif frequency == 'a':
+            # Extract annual data only
+            ff3_factors.drop(ff3_factors.index[:annual_factor_index_loc[0]],
+                             inplace=True)
 
-        # Ignore copyright footer & first 2 header rows
-        ff3_factors = ff3_factors.iloc[2:-1]
-        ff3_factors.reset_index(inplace=True)
-        ff3_factors.drop(columns=ff3_factors.columns[0], inplace=True)
+            # Ignore copyright footer & first 2 header rows
+            ff3_factors = ff3_factors.iloc[2:-1]
+            ff3_factors.reset_index(inplace=True)
+            ff3_factors.drop(columns=ff3_factors.columns[0], inplace=True)
 
-        # Deal with spacing issues (e.g. '  1927' instead of '1927')
-        ff3_factors['date_ff_factors'] = ff3_factors['date_ff_factors'].apply(
-            lambda x : x.strip())
+            # Deal with spacing issues (e.g. '  1927' instead of '1927')
+            ff3_factors['Date'] = ff3_factors['Date'].apply(
+                lambda x : x.strip())
 
-        # Convert dates to datetime objects (note: values will be int64)
-        ff3_factors['date_ff_factors'] = pd.to_datetime(ff3_factors['date_ff_factors'],
-                                                        format='%Y').dt.year.values
+            # Convert dates to datetime objects
+            ff3_factors['Date'] = pd.to_datetime({ff3_factors['Date']}+"1231",
+                                                            format='%Y%m%d')
 
 
     # Convert all factors to numeric and decimals (%)
@@ -109,55 +134,67 @@ def famaFrench3Factor(frequency='m'):
     return ff3_factors
 
 
-def momentumFactor(frequency='m'):
+def momentumFactor(frequency='d'):
     '''
     Returns the Momentum factor
 
     Set frequency as:
+        'd' for daily factors
         'm' for monthly factors
         'a' for annual factors
 
     '''
     rows_to_skip = 13
-    mom_raw_data = momAndOthers_dict['csv_links'][0]
 
-    mom_factor = pd.read_csv(mom_raw_data, skiprows=rows_to_skip)
-    mom_factor.rename(columns = {mom_factor.columns[0] : 'date_ff_factors'},
-                      inplace=True)
+    if frequency == 'd':
+        mom_raw_data = momAndOthers_dict['csv_links'][1]
+        mom_factor = pd.read_csv(mom_raw_data, skiprows=rows_to_skip)
+        mom_factor.rename(columns = {mom_factor.columns[0] : 'Date'},
+                          inplace=True)
+        # Ignore copyright footer
+        mom_factor = mom_factor.iloc[:-2]
+        mom_factor['Date'] = pd.to_datetime(mom_factor['Date'],
+                                                    format='%Y%m%d')
+    else:
+        mom_raw_data = momAndOthers_dict['csv_links'][0]
 
-    # Get index of annual factor returns
-    annual_factor_index_loc = mom_factor[
-            mom_factor.values == 'Annual Factors:'].index
-    # Clean annual and monthly versions
-    if frequency == 'm':
-        # Exclude annual factor returns
-        mom_factor.drop(mom_factor.index[annual_factor_index_loc[0]:], inplace=True)
+        mom_factor = pd.read_csv(mom_raw_data, skiprows=rows_to_skip)
+        mom_factor.rename(columns = {mom_factor.columns[0] : 'Date'},
+                          inplace=True)
 
-        # Convert dates to pd datetime objects
-        mom_factor['date_ff_factors'] = pd.to_datetime(mom_factor['date_ff_factors'],
-                                                       format='%Y%m')
+        # Get index of annual factor returns
+        annual_factor_index_loc = mom_factor[
+                mom_factor.values == 'Annual Factors:'].index
+        # Clean annual and monthly versions
+        if frequency == 'm':
+            # Exclude annual factor returns
+            mom_factor.drop(mom_factor.index[annual_factor_index_loc[0]:], inplace=True)
 
-        # Shift dates to end of month
-        mom_factor['date_ff_factors'] = mom_factor['date_ff_factors'].apply(
-            lambda date : date + relativedelta(day = 1, months = +1, days = -1))
+            # Convert dates to pd datetime objects
+            mom_factor['Date'] = pd.to_datetime(mom_factor['Date'],
+                                                           format='%Y%m')
 
-    elif frequency == 'a':
-        # Extract annual data only
-        mom_factor.drop(mom_factor.index[:annual_factor_index_loc[0]],
-                        inplace=True)
+            # Shift dates to end of month
+            mom_factor['Date'] = mom_factor['Date'].apply(
+                lambda date : date + relativedelta(day = 1, months = +1, days = -1))
 
-        # Ignore copyright footer & first 2 header rows
-        mom_factor = mom_factor.iloc[3:-1]
-        mom_factor.reset_index(inplace=True)
-        mom_factor.drop(columns=mom_factor.columns[0], inplace=True)
+        elif frequency == 'a':
+            # Extract annual data only
+            mom_factor.drop(mom_factor.index[:annual_factor_index_loc[0]],
+                            inplace=True)
 
-        # Deal with spacing issues (e.g. '  1927' instead of '1927')
-        mom_factor['date_ff_factors'] = mom_factor['date_ff_factors'].apply(
-            lambda x : x.strip())
+            # Ignore copyright footer & first 2 header rows
+            mom_factor = mom_factor.iloc[3:-1]
+            mom_factor.reset_index(inplace=True)
+            mom_factor.drop(columns=mom_factor.columns[0], inplace=True)
 
-        # Convert dates to datetime objects (note: values will be int64)
-        mom_factor['date_ff_factors'] = pd.to_datetime(mom_factor['date_ff_factors'],
-                                                       format='%Y').dt.year.values
+            # Deal with spacing issues (e.g. '  1927' instead of '1927')
+            mom_factor['Date'] = mom_factor['Date'].apply(
+                lambda x : x.strip())
+
+        # Convert dates to datetime objects
+        mom_factor['Date'] = pd.to_datetime(mom_factor['Date']+"1231",
+                                                       format='%Y%m%d')
 
     # Convert all factors to numeric and decimals (%)
     for col in mom_factor.columns[1:]:
@@ -169,76 +206,86 @@ def momentumFactor(frequency='m'):
     return mom_factor
 
 
-
-def carhart4Factor(frequency='m'):
+def carhart4Factor(frequency='d'):
     '''
     Returns Fama French 3 factors (Market Risk Premium, SMB, HML), and Momentum
 
     Set frequency as:
+        'd' for daily factors
         'm' for monthly factors
         'a' for annual factors
     '''
+    if frequency == 'd':
+        ff3_factors = famaFrench3Factor(frequency='d')
+        mom_factor = momentumFactor(frequency='d')
 
-    if frequency == 'm':
+    elif frequency == 'm':
         ff3_factors = famaFrench3Factor(frequency='m')
         mom_factor = momentumFactor(frequency='m')
 
-        carhart_4_factor = pd.merge(ff3_factors, mom_factor,
-                                    on='date_ff_factors', how='left')
     elif frequency == 'a':
         ff3_factors = famaFrench3Factor(frequency='a')
         mom_factor = momentumFactor(frequency='a')
 
-        carhart_4_factor = pd.merge(ff3_factors, mom_factor,
-                                    on='date_ff_factors', how='left')
-
+    carhart_4_factor = pd.merge(ff3_factors, mom_factor,
+                                    on='Date', how='left')
     return carhart_4_factor
 
 
-def famaFrench5Factor(frequency='m'):
+def famaFrench5Factor(frequency='d'):
     '''
     Returns Fama French 5 factors (Market Risk Premium, SMB, HML, RMW, CMA),
     and the risk-free rate (RF)
     '''
     rows_to_skip = 3
-    ff5_raw_data = ff3factor_dict['csv_links'][3]
 
-    ff5_factors = pd.read_csv(ff5_raw_data, skiprows=rows_to_skip)
-    ff5_factors.rename(columns = {ff5_factors.columns[0] : 'date_ff_factors'},
-                       inplace=True)
+    if frequency == 'd':
+        ff5_raw_data = ff_factor_dict['csv_links'][4]
+        ff5_factors = pd.read_csv(ff5_raw_data, skiprows=rows_to_skip)
+        ff5_factors.rename(columns={ff5_factors.columns[0]: 'Date'},
+                           inplace=True)
+        # Ignore copyright footer     
+        ff5_factors = ff5_factors.iloc[:-2]
+        ff5_factors['Date'] = pd.to_datetime(ff5_factors['Date'],
+                                                    format='%Y%m%d')
+    else:
+        ff5_raw_data = ff_factor_dict['csv_links'][3]
+        ff5_factors = pd.read_csv(ff5_raw_data, skiprows=rows_to_skip)
+        ff5_factors.rename(columns = {ff5_factors.columns[0] : 'Date'},
+                           inplace=True)
 
-    # Get index of annual factor returns
-    annual_factor_index_loc = ff5_factors[
-            ff5_factors.values == ' Annual Factors: January-December '].index
+        # Get index of annual factor returns
+        annual_factor_index_loc = ff5_factors[
+                ff5_factors.values == ' Annual Factors: January-December '].index
 
-    # Clean annual and monthly versions
-    if frequency == 'm':
-        ff5_factors.drop(ff5_factors.index[annual_factor_index_loc[0]:], inplace=True)
+        # Clean annual and monthly versions
+        if frequency == 'm':
+            ff5_factors.drop(ff5_factors.index[annual_factor_index_loc[0]:], inplace=True)
 
-        # Convert dates to pd datetime objects
-        ff5_factors['date_ff_factors'] = pd.to_datetime(ff5_factors['date_ff_factors'],
-                                                        format='%Y%m')
-        # Shift dates to end of month
-        ff5_factors['date_ff_factors'] = ff5_factors['date_ff_factors'].apply(
-            lambda date : date + relativedelta(day = 1, months = +1, days = -1))
+            # Convert dates to pd datetime objects
+            ff5_factors['Date'] = pd.to_datetime(ff5_factors['Date'],
+                                                            format='%Y%m')
+            # Shift dates to end of month
+            ff5_factors['Date'] = ff5_factors['Date'].apply(
+                lambda date : date + relativedelta(day = 1, months = +1, days = -1))
 
-    elif frequency == 'a':
-        # Extract annual data only
-        ff5_factors.drop(ff5_factors.index[:annual_factor_index_loc[0]],
-                         inplace=True)
+        elif frequency == 'a':
+            # Extract annual data only
+            ff5_factors.drop(ff5_factors.index[:annual_factor_index_loc[0]],
+                             inplace=True)
 
-        # Ignore copyright footer & first 2 header rows
-        ff5_factors = ff5_factors.iloc[2:]
-        ff5_factors.reset_index(inplace=True)
-        ff5_factors.drop(columns=ff5_factors.columns[0], inplace=True)
+            # Ignore copyright footer & first 2 header rows
+            ff5_factors = ff5_factors.iloc[2:]
+            ff5_factors.reset_index(inplace=True)
+            ff5_factors.drop(columns=ff5_factors.columns[0], inplace=True)
 
-        # Deal with spacing issues (e.g. '  1927' instead of '1927')
-        ff5_factors['date_ff_factors'] = ff5_factors['date_ff_factors'].apply(
-            lambda x : x.strip())
+            # Deal with spacing issues (e.g. '  1927' instead of '1927')
+            ff5_factors['Date'] = ff5_factors['Date'].apply(
+                lambda x : x.strip())
 
-        # Convert dates to datetime objects (note: values will be int64)
-        ff5_factors['date_ff_factors'] = pd.to_datetime(ff5_factors['date_ff_factors'],
-                                                        format='%Y').dt.year.values
+            # Convert dates to datetime objects (note: values will be int64)
+            ff5_factors['Date'] = pd.to_datetime(ff5_factors['Date']+"1231",
+                                                            format='%Y%m%d')
 
     # Convert all factors to numeric and decimals (%)
     for col in ff5_factors.columns[1:]:
